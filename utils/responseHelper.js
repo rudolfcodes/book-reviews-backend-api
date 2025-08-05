12a5873c918697f031a499b55c1a3ea2d462aac6
Rudolf Cardol <r_cardol@hotmail.com>
2025-08-05 14:47:32 +0100

    added statusCode to responseHelper

--- utils/responseHelper.js
+++ utils/responseHelper.js
@@ -1,4 +1,4 @@
-const sendSuccess = (res, data, message = "Success") => {
+const sendSuccess = (res, data, message = "Success", statusCode = 200) => {
   res.status(200).json({
     success: true,
     message,
