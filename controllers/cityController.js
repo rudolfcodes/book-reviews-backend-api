85d7ffef942f162fbc7509bd4ec4297dfa6ff8f8
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-14 19:18:14 +0100

    add city controller

---
+++ controllers/cityController.js
@@ -0,0 +1,39 @@
+const { sendError, sendSuccess } = require("../utils/responseHelper");
+const SWISS_CITIES = require("../data/swissCities.json");
+
+exports.getCitySuggestions = async (req, res, next) => {
+  try {
+    const { q } = req.query;
+    if (!q || q.trim() === "") {
+      return res.status(400).json({ error: "Query parameter 'q' is required" });
+    }
+
+    const postalCodePattern = /^\d{4}$/;
+    if (postalCodePattern.test(q)) {
+      const city = SWISS_CITIES.find((city) => city.postalCode === q);
+      if (city) {
+        return sendSuccess(
+          res,
+          [city],
+          "City suggestions fetched successfully",
+          200
+        );
+      } else {
+        return sendSuccess(
+          res,
+          [],
+          "No city found for the given postal code",
+          200
+        );
+      }
+    }
+    const query = q.toLowerCase();
+    const suggestions = SWISS_CITIES.filter((city) =>
+      city.name.toLowerCase().startsWith(query)
+    );
+    sendSuccess(res, suggestions, "City suggestions fetched successfully", 200);
+  } catch (error) {
+    next(error);
+    sendError(res, error, 500, "Failed to fetch city suggestions");
+  }
+};
