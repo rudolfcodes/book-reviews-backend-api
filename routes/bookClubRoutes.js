0de72acc3042f77b4f1a6e898c9ec8a0eb4ee500
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-18 19:15:06 +0100

    fix routing order

--- routes/bookClubRoutes.js
+++ routes/bookClubRoutes.js
@@ -5,6 +5,7 @@ const auth = require("../middlewares/auth");
 
 // PUBLIC ROUTES:
 router.get("/", bookClubController.getBookClubs); // Browse clubs
+router.get("/popular", bookClubController.getPopularBookClubs);
 router.get("/:clubId", bookClubController.getBookClubById); // Club details
 
 // AUTHENTICATED ROUTES:
@@ -13,8 +14,6 @@ router.post("/:clubId/join", auth, bookClubController.joinBookClub);
 router.post("/:clubId/leave", auth, bookClubController.leaveBookClub);
 router.put("/:clubId/rsvp", auth, bookClubController.rsvpToMeeting);
 router.get("/:clubId/members", auth, bookClubController.getClubMembers);
-router.get("/popular", bookClubController.getPopularBookClubs);
-
 // ADMIN ONLY ROUTES:
 router.put("/:clubId", auth, bookClubController.updateBookClub); // Need to add admin check
 router.delete("/:clubId", auth, bookClubController.deleteBookClub); // Need to add admin check
