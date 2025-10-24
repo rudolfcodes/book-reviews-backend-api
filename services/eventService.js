92ebac64c1cde28b9f57785c0adac09290f09bd8
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-24 17:13:21 +0100

    getEvents service

---
+++ services/eventService.js
@@ -0,0 +1,59 @@
+const Event = require("../models/Event");
+
+class EventService {
+  async getEvents(filters, options) {
+    const { page, limit, sortBy } = options;
+    const { location, language, title, dateRange } = filters;
+    const filter = {};
+
+    if (location) filter.location = location;
+    if (language) filter.language = language;
+    if (title) filter.title = { $regex: title, $options: "i" }; // Case-insensitive search
+    if (dateRange) {
+      filter.date = {};
+      if (dateRange.from) filter.date.$gte = new Date(dateRange.from);
+      if (dateRange.to) filter.date.$lte = new Date(dateRange.to);
+    }
+
+    let sortCriteria;
+    switch (sortBy) {
+      case "date":
+        sortCriteria = { date: 1 };
+        break;
+      case "newest":
+        sortCriteria = { createdAt: -1 };
+        break;
+      case "oldest":
+        sortCriteria = { createdAt: 1 };
+        break;
+      case "title":
+        sortCriteria = { title: 1 };
+        break;
+      default:
+        sortCriteria = { date: 1 };
+        break;
+    }
+
+    const pageNum = parseInt(page);
+    const limitNum = parseInt(limit);
+    const skip = (pageNum - 1) * limitNum;
+    const totalDocs = await Event.countDocuments(filter);
+    const totalPages = Math.ceil(totalDocs / limitNum);
+    const docs = await Event.find(filter)
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limitNum);
+
+    return {
+      docs,
+      totalDocs,
+      limit: limitNum,
+      totalPages: totalPages,
+      currentPage: pageNum,
+      hasNextPage: pageNum < totalPages,
+      hasPrevPage: pageNum > 1,
+    };
+  }
+}
+
+module.exports = new EventService();
