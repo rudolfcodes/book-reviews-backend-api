87afd4e585d35940758669347a93185fd0afa9da
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-24 18:18:33 +0100

    remove event routes
    events are part of clubs so this route was a mistake

--- routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../middlewares/auth");
-const eventController = require("../controllers/eventController");
-
-router.get("/", eventController.getEvents);
-router.post("/", auth, eventController.createEvent);
-
-module.exports = router;
