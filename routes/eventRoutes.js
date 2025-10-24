98abea2e83bf16699959360cd1502e17a8bfa75d
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-24 18:08:51 +0100

    add route for event creation

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middlewares/auth");
 const eventController = require("../controllers/eventController");
 
 router.get("/", eventController.getEvents);
+router.post("/", auth, eventController.createEvent);
 
 module.exports = router;
