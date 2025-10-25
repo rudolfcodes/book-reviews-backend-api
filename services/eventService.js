bc8438e204d33f4fef74cc790b8c56b8381e8b43
Rudolf Cardol <r_cardol@hotmail.com>
2025-10-25 16:01:39 +0100

    fix missing mongoose import

--- services/eventService.js
+++ services/eventService.js
@@ -1,5 +1,6 @@
 const Event = require("../models/Event");
 const User = require("../models/User");
+const mongoose = require("mongoose");
 
 class EventService {
   async getEvents(filters, options) {
