e6ed1f971b189959c4529edd47a23a2d26d183db
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-13 11:09:46 +0000

    add getEventStatus to controller

--- controllers/eventStatusController.js
+++ controllers/eventStatusController.js
@@ -15,3 +15,13 @@ exports.updateExpiredEvents = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getEventStats = async (req, res, next) => {
+  try {
+    const stats = await eventStatusService.getEventStats();
+    sendSuccess(res, stats, "Event statistics retrieved successfully", 200);
+  } catch (error) {
+    sendError(res, error, 500, "Failed to retrieve event statistics");
+    next(error);
+  }
+};
